42dcde9c606e8f9bfaca387ac98504e904c10814
SSANTA11 <ssanta011205@skuniv.ac.kr>
2026-01-30 02:20:38 +0900

    01/30 스프링 학습 일지 학습 내용 Optional을 사용한 null값 에러 방지 findAny를 활용한 예외상황이 없는 데이터 반환
    문제:
    정상적인 테스트 실행이 불가
    'Exit value 1' 문제로, ide 코드 좌측 테스트 실행 버튼 클릭 시, 비정상 프로세스 종료 지속 발생
    
    시도:
    IntelliJ의 테스트 주체를Gradle에서 IntelliJ로 바꿔보자
    
    해결:
    IntelliJ로 사용하여 문제 해결

--- src/main/java/com/example/controller/MyController.java
+++ src/main/java/com/example/controller/MyController.java
@@ -44,4 +44,3 @@ public class MyController {
     }
 
 }
-r
--- src/main/java/com/example/repository/MemoryMemberRepository.java
+++ src/main/java/com/example/repository/MemoryMemberRepository.java
@@ -6,7 +6,8 @@ import java.util.*;
 
 public class MemoryMemberRepository implements MemberRepository {
     private static final Map<Long, Member> store = new HashMap<>();
-    private static long sequence =0L;
+    private static long sequence = 0L;
+
     @Override
     public Member save(Member member) {
         member.setId(++sequence);
@@ -20,13 +21,17 @@ public class MemoryMemberRepository implements MemberRepository {
     }
 
     @Override
-    public Optional<Member> findByName(Long name) {
-        return store.values().stream().filter(member->member.getName().equals(name)).findAny();
-
+    public Optional<Member> findByName(String name) {
+        return store.values().stream().filter(
+                member -> member.getName().equals(name)).findAny();
     }
 
     @Override
     public List<Member> findAll() {
         return new ArrayList<>(store.values());
+    }
+
+    public void clearStore() {
+        store.clear();
     }
 }
